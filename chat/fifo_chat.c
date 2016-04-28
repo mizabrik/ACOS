4912e52b0b6920ee0d53a250cdf8639518e7d7cc
Fedor Moiseev <femoiseev@gmail.com>
2016-04-28 17:58:34 +0300

    Solve problem with output order.

--- chat/fifo_chat.c
+++ chat/fifo_chat.c
@@ -8,9 +8,9 @@
 #include <errno.h>
 #include <assert.h>
 
-#define sem_write_name "/sem_write"
-#define sem_first_name "/sem_first_process"
-#define sem_second_name "/sem_second_process"
+//#define sem_write_name "/sem_write"
+//#define sem_first_name "/sem_first_process"
+//#define sem_second_name "/sem_second_process"
 #define SEM_WRITE "/sem_write"
 #define SEM_FIRST_PROCESS "/sem_first_process"
 #define SEM_SECOND_PROCESS "/sem_second_process"
@@ -49,10 +49,11 @@ struct my_fifo {
   int fifo_fd;
 };
 
-static void *read_from_fifo(__attribute__((unused)) void *void_ptr) {
+static void *read_from_fifo(void *void_ptr) {
+ 	struct my_fifo *fifo;
+	fifo = (struct my_fifo*) void_ptr;
+	//printf("read thread: sem_write(%p), sem_read(%p)\n", fifo->sem_write, fifo->sem_read);
   do {
-		struct my_fifo *fifo;
-		fifo = (struct my_fifo*) void_ptr;
 		sem_wait(fifo->sem_read);
 		char* c;
 		c = (char*) malloc(1);
@@ -62,21 +63,23 @@ static void *read_from_fifo(__attribute__((unused)) void *void_ptr) {
 	  	sem_post(fifo->sem_write);
 		}
 		free(c);
-  } while(!feof(stdin));
+  } while(1);//!feof(stdin));
   return NULL;
 }
 
 int main(int argc, char *argv[]) {
-  assert(argc == 2);
+  if(argc != 2) {
+		error(EXIT_FAILURE, 0, "Wrong number of arguments.");
+	}
 
   int fifo_fd;
   int process_number = 1;
-  struct stat *fifo_stat;
+  struct stat fifo_stat;
 	int stat_res;
-	stat_res = stat(argv[1], fifo_stat);
+	stat_res = stat(argv[1], &fifo_stat);
 	
 	if(stat_res == 0) {
-		if(!S_ISFIFO(fifo_stat->st_mode)) {
+		if(!S_ISFIFO(fifo_stat.st_mode)) {
 	  	error(EXIT_FAILURE, 0, "%s exists.", argv[1]);
 		}
 	} else if (errno == ENOENT) {
@@ -85,11 +88,14 @@ int main(int argc, char *argv[]) {
 	  error(EXIT_FAILURE, 0, "Something wrong with %s.", argv[1]);
 	}
 
+	//printf("%d\n", process_number);
+
   if(process_number == 0) {
   	mkfifo(argv[1], 0600);
   }
+
   errno = 0;
-	printf("%d\n", process_number);
+	// printf("%d\n", process_number);
 
   fifo_fd = open(argv[1], O_RDWR, O_CREAT);
   if (errno != 0 && errno != EEXIST) {
@@ -100,23 +106,27 @@ int main(int argc, char *argv[]) {
   sem_t* sem_first;
   sem_t* sem_second;
 
-  /*char sem_write_name[255];
+  char sem_write_name[255];
   char sem_first_name[255];
   char sem_second_name[255];
 
   snprintf(sem_write_name, 255, "%s%s", SEM_WRITE, argv[1]);
   snprintf(sem_first_name, 255, "%s%s", SEM_FIRST_PROCESS, argv[1]);
   snprintf(sem_second_name, 255, "%s%s", SEM_SECOND_PROCESS, argv[1]);
-	printf("%s", sem_write_name);*/
+	//printf("%s", sem_write_name);
 
 	errno = 0;
   if(process_number == 0) {
 		sem_unlink(sem_write_name);
   	sem_unlink(sem_first_name);
   	sem_unlink(sem_second_name);
+		errno = 0;
 		sem_write = sem_open(sem_write_name, O_CREAT | O_EXCL, 0600, 0);
 		sem_first = sem_open(sem_first_name, O_CREAT | O_EXCL, 0600, 0);
 		sem_second = sem_open(sem_second_name, O_CREAT | O_EXCL, 0600, 0);
+		if (errno) {
+			error(EXIT_FAILURE, 0, "Problems with synchronisation.");
+		}
   } else {
 		sem_write = sem_open(sem_write_name, 0);
 		sem_first = sem_open(sem_first_name, 0);
@@ -129,20 +139,17 @@ int main(int argc, char *argv[]) {
 		not_correct_values += sem_val;
 		sem_getvalue(sem_second, &sem_val);
 		not_correct_values += sem_val;
-		if(not_correct_values) {
-			error(EXIT_FAILURE, 0, "Problems with synchronisation.");
-		}
 		sem_post(sem_write);
 		sem_unlink(sem_write_name);
 		sem_unlink(sem_first_name);
 		sem_unlink(sem_second_name);
 		unlink(argv[1]);
+		if(not_correct_values || errno) {
+			error(EXIT_FAILURE, 0, "Problems with synchronisation.");
+		}
 	}
-
-	if (errno) {
-			perror(EXIT_FAILURE, 0, "Problems with synchronisation.");
-			errno = 0;
-	}
+	errno = 0;
+	//printf("write thread: sem_write(%p), sem_first(%p), sem_second(%p)\n", sem_write, sem_first, sem_second);
 
   struct my_fifo *fifo;
 	fifo = (struct my_fifo*) malloc (sizeof(struct my_fifo));
@@ -157,15 +164,15 @@ int main(int argc, char *argv[]) {
 	pthread_t thread;
 
   if(pthread_create(&thread, NULL, read_from_fifo, fifo) != 0) {
-		return EXIT_FAILURE;
+		error(EXIT_FAILURE, 0, "Problems with threads.");
   }
 
   do {
 		char* str;
 		get_line(&str, stdin);
+		//printf(str);
 		sem_wait(sem_write);
-		char* c;
-		c = (char*) malloc(1);
+		char c[1];
 		int i;
 		for(i = 0; i < strlen(str); ++i) {
 	  	c[0] = str[i];
@@ -183,7 +190,12 @@ int main(int argc, char *argv[]) {
 		} else {
 	  	sem_post(sem_first);
 		}
-  } while(!feof(stdin));
+		int t;
+		/*sem_getvalue(sem_first, &t);
+		printf("sem_first: %d\n", t);
+		sem_getvalue(sem_second, &t);
+		printf("sem_second: %d\n", t);*/
+  } while(1);//!feof(stdin));
 
   sem_close(sem_write);
   sem_close(sem_first);
