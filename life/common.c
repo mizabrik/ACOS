486e2906f928089d79e24a14d44da74642f6ff1d
Alexander Vasilyev <mizabrik@gmail.com>
2016-05-25 16:31:49 +0300

    add common code for life with threads and forks

---
+++ life/common.c
@@ -0,0 +1,47 @@
+#include "common.h"
+
+#include <assert.h>
+#include <semaphore.h>
+
+#include "life.h"
+
+void coordinator(life_t *life, life_t *tmp, unsigned steps, unsigned n_workers,
+                 sem_t jobs, sem_t ready) {
+  unsigned step;
+  unsigned i;
+
+  assert(n_workers > 0);
+  
+  for (step = 0; step < steps; ++steps) {
+    for (i = 0; i < n_workers; ++i)
+      sem_post(jobs);
+    for (i = 0; i < n_workers; ++i)
+      sem_wait(ready);
+
+    swap_lifes(tmp, life); 
+  }
+
+  swap_lifes(tmp, life); 
+}
+
+void worker(worker_data_t *data) {
+
+  for (;;) {
+    int x, y;
+
+    sem_wait(data->jobs);
+
+    for (y = data->y_begin; y < data->y_end; ++y)
+      for (x = 0; x < data->new->width; ++x)
+        update_cell(data->new, data->old, x, y);
+
+    sem_post(data->ready);
+  }
+}
+
+void swap_lifes(life_t *a, life_t *b) {
+  life_t tmp;
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
--- life/common.h
+++ life/common.h
@@ -1,6 +1,31 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+#include <semaphore.h>
+#include <pthread.h>
 
+#include "life.h"
+
+struct worker_data {
+  pthread_t thread;
+
+  life_t *old;
+  life_t *new;
+
+  int y_begin;
+  int y_end;
+  sem_t *jobs;
+  sem_t *ready; 
+};
+typedef struct worker_data worker_data_t;
+
+
+/* requires */
+void coordinator(life_t *life, life_t *tmp, unsigned steps, unsigned n_workers,
+                 sem_t jobs, sem_t ready);
+
+void worker(struct worker_data *data);
+
+void swap_lifes(life_t *a, life_t *b);
 
 #endif /* COMMON_H_ */
